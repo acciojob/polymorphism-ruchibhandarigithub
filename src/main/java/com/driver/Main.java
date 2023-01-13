d825fbe3cca01133121b9f1a3779827c49307200
ruchibhandarigithub <bhandariruchi498@gmail.com>
2023-01-13 04:23:44 -0800

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,33 @@
 package com.driver;
 
+import java.util.Scanner;
 public class Main {
+   public static  void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       int x = sc.nextInt();
+       int y = sc.nextInt();
+       int z = sc.nextInt();
+       double x1 = sc.nextDouble();
+       double y1 = sc.nextDouble();
+       Product p = new Product();
+
+       System.out.println(p.product(x,y));
+       System.out.println(p.product(x,y,z));
+       System.out.println(p.product(x1,y1));
+
+
+   }
+}
+class Product{
+    public int product(int x, int y) {
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+    public double product(double x,double y){
+        return x*y;
+    }
+
 
 }
