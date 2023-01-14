3fd43fdd1fed4dd64c641dd5bbae96cd3d864430
ruchibhandarigithub <bhandariruchi498@gmail.com>
2023-01-14 01:06:38 -0800

    ruchi

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,17 @@
 package com.driver;
 
 public class Main {
+  public static class Product{
+      public int product(int x,int y){
+          return x*y;
+      }
+      public int product(int x,int y,int z){
+          return x*y*z;
+      }
+      public double product(double x,double y){
+          return x*y;
+      }
+  }
    public static  void main(String args[]){
      Product p = new Product();
      p.product(1,2);
@@ -8,17 +19,4 @@ public class Main {
      p.product(1.5,1.5);
 
    }
-}
-class Product{
-    public int product(int x, int y) {
-        return x*y;
-    }
-    public int product(int x,int y,int z){
-        return x*y*z;
-    }
-    public double product(double x,double y){
-        return x*y;
-    }
-
-
 }
